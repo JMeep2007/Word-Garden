0bddffcaa7828c1c7bd81d943f35fc268eed1898
JMeep2007 <meepegamajulian1@gmail.com>
2026-01-13 11:36:24 -0800

    Initial Commit

---
+++ Word Garden/Word_GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_GardenApp.swift
+//  Word Garden
+//
+//  Created by MEEPEGAMA, JULIAN on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Word_GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
